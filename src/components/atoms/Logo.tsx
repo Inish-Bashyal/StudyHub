385200c08c9b67135c87e41f594c57cac23e3499
Inish-Bashyal <inish.bashyal99@gmail.com>
2024-06-10 20:29:03 +0545

    layout finalised

---
+++ src/components/atoms/Logo.tsx
@@ -0,0 +1,8 @@
+import logoImg from 'src/assets/images/logo.jpeg';
+const Logo = () => (
+  <span className="ml-2 self-center whitespace-nowrap text-2xl font-bold text-gray-900 dark:text-white md:text-xl">
+    Study Hub International Education Consultancy
+  </span>
+);
+
+export default Logo;
